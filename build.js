dbcee146426f577cbe87944a9b4fb0acddb39175
Simon Dalvai <s.dalvai@noi.bz.it>
2022-06-17 09:25:03 +0200

    create on single js file as build

--- build.js
+++ build.js
@@ -3,13 +3,11 @@ const concat = require('concat');
 
 (async function build() {
   const files = [
-    './dist/runtime.js',
     './dist/polyfills.js',
     './dist/main.js'
   ];
 
   await concat(files, './dist/webcomp-ecar-friendly-accommodations.min.js');
-  await fs.remove('./dist/runtime.js')
   await fs.remove('./dist/polyfills.js')
   await fs.remove('./dist/main.js')
 })();
